4d3d600ebe290d5bf0d2fec2de8a9311f3dc11a0
donkeykong91 <darylnator@gmail.com>
2018-07-28 05:32:30 -0700

    Added express, http, and socketio, http server listening on port 3000, express listening for '/' route, socketio listening for users

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-const app = require("express")();
+const app           = require("express")();
 
-const server = require("http").Server(app);
+const server        = require("http").Server(app);
 
-const io = require("socket.io")(server);
+const io            = require("socket.io")(server);
 
-const port = 3000;
+const port          = 3000;
+
+
+server.listen(port, function () {
+
+    console.log(`Server is running on port ${port}`);
+    
+});
+
+
+app.get("/", function (request, respond) {
+
+    respond.sendFile(__dirname + "/public/index.html");
+
+});
+
+io.on("connection", function (socket) {
+
+
+    console.log("User connected");
+
+    socket.emit("message", {manny: "Hey how are you?"});
+
+    
+    socket.on("another event", function (data) {
+
+        console.log(data);
+
+    });
+
+});
